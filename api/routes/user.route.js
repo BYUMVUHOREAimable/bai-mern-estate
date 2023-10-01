b1dce74b353a69075ce4070213023864f8c81cdc
BYUMVUHOREAimable <122086815+BYUMVUHOREAimable@users.noreply.github.com>
2023-10-01 19:17:46 +0200

    complete app

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
-
 const router = express.Router();
 
 router.get('/test', test);
